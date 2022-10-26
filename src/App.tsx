e7d5a6487c68b529f86d16658414a068f7026a83
Steffen Geving <GEVIS1@student.op.ac.nz>
2022-10-26 22:03:08 +1300

    feat: make app draw header
    currently only the header exists, so draw the header

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@
  * The main component that renders in place of the #root div when react populates the DOM
  */
 
+import Header from './components/Header/Header';
+
 /**
  * The app component contains the header, navbar and dashboard.
  */
-const App = () => <div>Hello World!</div>;
+const App = () => <Header />;
 
 export default App;
