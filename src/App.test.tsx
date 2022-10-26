aad409162118910a6327cb416a89e15dba653edd
Steffen Geving <GEVIS1@student.op.ac.nz>
2022-10-26 20:20:49 +1300

    test(app.test.tsx): correct test name

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders hello world', () => {
   render(<App />);
   const helloWorld = screen.getByText(/Hello World!/i);
   expect(helloWorld).toBeInTheDocument();
