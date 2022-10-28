b527d7bda4a86f236cc3b467a3c3774f7ee2d918
Steffen Geving <GEVIS1@student.op.ac.nz>
2022-10-29 11:25:57 +1300

    refactor(header): use styled components
    replaced divs with Container component and used styled components to make components transparent

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,36 +1,106 @@
 /**
- * The header component renders six icon components from material-ui/icons and sits at the top of the page
+ * The header component renders six icon components from material-ui/icons and sits at the top of the page.
+ * In the header component I have decided to use styled components for the styling of everything.
  */
 
 import {
-  FlashOn,
-  Home,
-  LiveTv,
-  PersonOutline,
-  Search,
-  VideoLibrary,
+  FlashOn as FlashOnOrig,
+  Home as HomeOrig,
+  LiveTv as LiveTvOrig,
+  PersonOutline as PersonOutlineOrig,
+  Search as SearchOrig,
+  VideoLibrary as VideoLibraryOrig,
 } from '@mui/icons-material';
+import {
+  BottomNavigation as BottomNavigationOrig,
+  BottomNavigationAction as BottomNavigationActionOrig,
+  Container as ContainerOrig,
+  styled,
+} from '@mui/material';
 
-const Header: React.FC = () =>
-  <div className="wrapper">
-    <div className="icon home" data="H O M E">
-      <Home />
-    </div>
-    <div className="icon flash-on" data="T R E N D I N G">
-      <FlashOn />
-    </div>
-    <div className="icon live-tv" data="V E R I F I E D">
-      <LiveTv />
-    </div>
-    <div className="icon video-library" data="A C C O U N T">
-      <VideoLibrary />
-    </div>
-    <div className="icon search" data="S E A R C H">
-      <Search />
-    </div>
-    <div className="icon person-outline" data="C O L L E C T I O N S">
-      <PersonOutline />
-    </div>
-  </div>
-;
+/**
+ * Style the background colour of the container
+ */
+const Container = styled(ContainerOrig)({
+  backgroundColor: 'transparent',
+});
+
+/**
+ * Style the background colour of the bottomnavigationaction and the colour of the label text
+ */
+const BottomNavigationAction = styled(BottomNavigationActionOrig)({
+  backgroundColor: 'transparent',
+  '& .MuiBottomNavigationAction-label': {
+    color: 'white',
+  },
+});
+
+/**
+ * Style the background colour of the bottomnavigation and the colour of the label text
+ */
+const BottomNavigation = styled(BottomNavigationOrig)({
+  backgroundColor: 'transparent',
+});
+
+/**
+ * Style all icons to be white
+ */
+const [Home, FlashOn, LiveTv, VideoLibrary, Search, PersonOutline] =
+  Object.values([
+    HomeOrig,
+    FlashOnOrig,
+    LiveTvOrig,
+    VideoLibraryOrig,
+    SearchOrig,
+    PersonOutlineOrig,
+  ]).map((value) =>
+    styled(value)({
+      color: 'white',
+    })
+  );
+
+const Header: React.FC = () => {
+  return (
+    <Container color="transparent">
+      <BottomNavigation>
+        <BottomNavigationAction
+          disableRipple
+          showLabel={true}
+          label="Home"
+          icon={<Home />}
+        />
+        <BottomNavigationAction
+          disableRipple
+          showLabel={false}
+          label="Trending"
+          icon={<FlashOn />}
+        />
+        <BottomNavigationAction
+          disableRipple
+          showLabel={false}
+          label="Verified"
+          icon={<LiveTv />}
+        />
+        <BottomNavigationAction
+          disableRipple
+          showLabel={false}
+          label="Account"
+          icon={<VideoLibrary />}
+        />
+        <BottomNavigationAction
+          disableRipple
+          showLabel={false}
+          label="Search"
+          icon={<Search />}
+        />
+        <BottomNavigationAction
+          disableRipple
+          showLabel={false}
+          label="Collections"
+          icon={<PersonOutline />}
+        />
+      </BottomNavigation>
+    </Container>
+  );
+};
 export default Header;
