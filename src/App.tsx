4594c9467697f34d959bc333be8e2401d12006e0
Steffen Geving <GEVIS1@student.op.ac.nz>
2022-10-30 12:23:15 +1300

    feat(app.tsx): add StyledEngineProvider
    added around app as a wrapper to ensure imported SASS modules get injected first to remove need for \!important keyword on every style declaration

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,27 @@
  * The main component that renders in place of the #root div when react populates the DOM
  */
 
+import { StyledEngineProvider } from '@mui/material/styles';
+
 import './App.sass';
 
 import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
 
 /**
  * The app component contains the header, navbar and dashboard.
  */
-const App = () => <Header />;
+const App = () =>
+  <>
+    {/**
+     * Using the StyledEngineProvider is necessary here to inject SASS modules before default styles. (Style Library Interoperability - Material UI, n.d.)
+     * Removing the need for !important markers on all style declarations.
+     */}
+    <StyledEngineProvider injectFirst>
+      <Header />
+      <Navbar />
+    </StyledEngineProvider>
+  </>
+;
 
 export default App;
