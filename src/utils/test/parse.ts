fb7957a6fbd3e9f5a3c86e29f0212ba24d48960a
Steffen Geving <GEVIS1@student.op.ac.nz>
2022-10-30 10:52:41 +1300

    feat: add test scripts
    these scripts were useful in determining the shape of the incoming data

---
+++ src/utils/test/parse.ts
@@ -0,0 +1,88 @@
+import fs from 'fs/promises';
+import { IMovieDBResponseResults } from '../axios';
+
+(async () => {
+  try {
+    const rawData = await fs.readFile('./output.json', { encoding: 'utf8' });
+
+    if (!rawData) {
+      throw Error('File data is empty.');
+    }
+    const data: IMovieDBResponseResults[] = JSON.parse(rawData);
+
+    type NumKeys = {
+      [index: string]: number;
+    };
+    type KeyValue = {
+      [index: string]: unknown;
+    };
+
+    const numKeys: NumKeys = {};
+    const keyValue: KeyValue = {};
+
+    /**
+     * Iterate the data to find the biggest object to use for shaping keyValue's sets
+     */
+    let biggestSample = {};
+    data.forEach((v) => {
+      if (Object.keys(v).length > Object.keys(biggestSample).length) {
+        biggestSample = v;
+      }
+    });
+
+    /**
+     * Construct the sets
+     */
+    Object.entries(biggestSample).forEach(([k]) => {
+      keyValue[k] = new Set();
+    });
+
+    data.forEach((movie) => {
+      Object.entries(movie).forEach(([k, v]) => {
+        if (!(keyValue[k] instanceof Set)) {
+          throw Error('Found key for which there is no set!');
+        }
+
+        /**
+         * If v is an array, insert each value separately
+         */
+        if (v instanceof Array) {
+          /* eslint-disable no-extra-parens */
+          v.forEach((w) => (keyValue[k] as Set<unknown>).add(w));
+        } else {
+          (keyValue[k] as Set<unknown>).add(v);
+          /* eslint-enable no-extra-parens */
+        }
+
+        if (numKeys[k] === undefined) {
+          numKeys[k] = 1;
+          return;
+        }
+        numKeys[k]++;
+        return;
+      });
+    });
+
+    // Trim huge sets
+    const keyValueTable: KeyValue = {};
+    Object.entries(keyValue).forEach(([k, v]) => {
+      if (v instanceof Set && v.size > 15) {
+        keyValueTable[k] = 'Sample size too big/Undefined';
+      } else {
+        keyValueTable[k] = Array.from(v as Set<unknown>);
+      }
+    });
+
+    /* eslint-disable no-console */
+    console.log('Finished successfully:');
+    console.table(numKeys);
+    console.table(keyValueTable);
+  } catch (error) {
+    console.log('Something went wrong.');
+    if (error instanceof Error) {
+      console.log(error.message);
+      console.table(error);
+    }
+    /* eslint-enable no-console */
+  }
+})();
---
+++ src/utils/test/sampledata.ts
@@ -0,0 +1,36 @@
+/**
+ * A quick download script to confirm expectations about the shape of the data.
+ */
+
+import axios from 'axios';
+import fs from 'fs/promises';
+import { IMovieDBResponse, IMovieDBResponseResults } from '../axios';
+
+(async () => {
+  const data: IMovieDBResponseResults[] = [];
+  const url =
+    'https://api.themoviedb.org/3/discover/movie?api_key=44a956178dd73105f015fb8978c09a47';
+  let i = 2;
+
+  try {
+    const firstRequest = await axios.get<IMovieDBResponse>(url);
+
+    data.push(...firstRequest.data.results);
+
+    for (; i < firstRequest.data.total_pages; i++) {
+      const { results } = await (await axios.get<IMovieDBResponse>(url)).data;
+      data.push(...results);
+
+      // eslint-disable-next-line no-console
+      if (i % 100 === 0) console.log(i);
+    }
+
+    await fs.writeFile('./output.json', JSON.stringify(data));
+  } catch (error) {
+    await fs.writeFile('./output.json', JSON.stringify(data));
+    // eslint-disable-next-line no-console
+    console.log(i);
+    // eslint-disable-next-line no-console
+    console.error('Crashed.');
+  }
+})();
