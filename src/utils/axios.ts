84e38de5985554666ed156668ecab70e2e17e121
Steffen Geving <GEVIS1@student.op.ac.nz>
2022-10-30 10:53:53 +1300

    feat(axios.ts): add MovieDBLanguages type

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -4,22 +4,27 @@ const instance = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
 });
 
+type MovieDBLanguages = 'en' | 'ja' | 'zh' | 'es' | 'fr';
+
+/**
+ * An assumed safe interface constructed from running the scripts in the test/ directory..
+ *
+ * Use sampledata.ts to get a sample set, and then parse.ts to get a quick overview of the data.
+ */
 export interface IMovieDBResponseResults {
   adult: boolean;
   backdrop_path: string;
   id: number;
   name: string;
-  original_language: string; // TODO: string literal?
+  original_language: MovieDBLanguages;
   original_name: string;
   overview: string;
   poster_path: string;
-  media_type: string; // TODO: string literal?
   genre_ids: number[];
   popularity: number;
   first_air_date: string;
   vote_average: number;
   vote_count: number;
-  origin_country: string[]; // TODO: string literal?
 }
 
 export interface IMovieDBResponse {
