91614051f9aca5a57d77edbdfed83d1b2dcf03e4
Steffen Geving <gevis1@student.op.ac.nz>
2022-11-02 15:28:09 +1300

    feat(app.tsx): add state for category

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,32 @@
  */
 
 import { StyledEngineProvider } from '@mui/material/styles';
+import { useState } from 'react';
 
 import './App.sass';
 
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Dashboard from './components/Dashboard/Dashboard';
+import { Category } from './utils/endpoints';
 
 /**
  * The app component contains the header, navbar and dashboard.
  */
-const App = () =>
-  <>
-    {/**
-     * Using the StyledEngineProvider is necessary here to inject SASS modules before default styles. (Style Library Interoperability - Material UI, n.d.)
-     * Removing the need for !important markers on all style declarations.
-     */}
-    <StyledEngineProvider injectFirst>
-      <Header />
-      <Navbar />
-      <Dashboard />
-    </StyledEngineProvider>
-  </>
-;
+const App = () => {
+  const [category, setCategory] = useState<Category>('Trending');
+  return (
+    <>
+      {/**
+       * Using the StyledEngineProvider is necessary here to inject SASS modules before default styles. (Style Library Interoperability - Material UI, n.d.)
+       * Removing the need for !important markers on all style declarations.
+       */}
+      <StyledEngineProvider injectFirst>
+        <Header />
+        <Navbar setCategory={setCategory} />
+        <Dashboard category={category} />
+      </StyledEngineProvider>
+    </>
+  );
+};
 export default App;
