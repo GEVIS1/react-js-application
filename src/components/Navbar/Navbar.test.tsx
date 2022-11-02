c784b4a0c0e6a80297cc899ff4a10b3a7a5f905c
Steffen Geving <gevis1@student.op.ac.nz>
2022-11-02 15:59:48 +1300

    test: update tests to match component changes

--- src/components/Navbar/Navbar.test.tsx
+++ src/components/Navbar/Navbar.test.tsx
@@ -12,7 +12,7 @@ afterEach(cleanup);
 
 describe('navbar tests', () => {
   test('renders navbar text from endpoints correctly', () => {
-    render(<Navbar />);
+    render(<Navbar category={'Trending'} setCategory={() => null} />);
 
     const buttons: HTMLButtonElement[] = [];
 
@@ -26,18 +26,39 @@ describe('navbar tests', () => {
   });
 
   test('navbar is clickable', async () => {
-    render(<Navbar />);
+    render(<Navbar category={'Trending'} setCategory={() => null} />);
 
     const buttons: HTMLElement[] = [];
 
+    /**
+     * Put all buttons in an array for testing
+     */
     endpoints.forEach(async (e) => {
-      buttons.push(await screen.getByText(e.type));
+      const b = await screen.getByText(e.type);
+
+      /**
+       * Check default classes
+       */
+      if (e.type === 'Trending') {
+        expect(b).toHaveClass('SelectedButton');
+        expect(b).not.toHaveClass('Button');
+      } else {
+        expect(b).toHaveClass('Button');
+        expect(b).not.toHaveClass('SelectedButton');
+      }
+
+      buttons.push(b);
     });
 
+    /**
+     * Check that classes are correctly applied as they are clicked
+     */
     for (const button in buttons) {
       if (Object.prototype.hasOwnProperty.call(buttons, button)) {
         const b = buttons[button];
         await userEvent.click(b);
+        expect(b).toHaveClass('SelectedButton');
+        expect(b).not.toHaveClass('Button');
       }
     }
   });
